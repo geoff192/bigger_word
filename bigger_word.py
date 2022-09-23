e50efb959b50cf5ed2c5570409c4a99e48d3db30
Geoffrey Aguero <geoffreyaguero@geoffreys-mbp.lan>
2022-09-23 09:28:35 -0600

    add py

---
+++ bigger_word.py
@@ -0,0 +1,22 @@
+alphabet="abcdefghijklmnopqrstuvwxyz"
+
+def bigger_word(w1, w2):
+    #Crear variables para almacenar la suma
+    w1_suma = 0
+    w2_suma = 0
+
+    #Recorrer cada letra de w1
+    for
+        #Obtener el indice de la letra actual
+
+        #Agregarlo a la suma de la primera palabra
+    
+
+    #Recorrer cada letra de w2
+    for
+        #Obtener el indice de la letra actual
+
+        #Agregarlo a la suma de la segunda palabra
+
+
+    #Retornar el mayor
